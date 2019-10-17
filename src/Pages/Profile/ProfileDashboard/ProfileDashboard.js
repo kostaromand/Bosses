7d9a2d77c27314053778e6330e273c289c111d42
Konstantin <kostaromand@gmail.com>
2019-10-17 12:42:01 +0600

    user summary structure changed

--- src/Pages/Profile/ProfileDashboard/ProfileDashboard.js
+++ src/Pages/Profile/ProfileDashboard/ProfileDashboard.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import Dashboard from '../../../Components/Dashboard'
 import TabContainer from './TabContainer'
-import ProfileDashboardGroup from './ProfileDashboardGroup'
+import DashboardGroup from '../../../Components/Dashboard/DashboardGroup'
+import UserSummary from './UserSummary'
+import { Link } from 'react-router-dom'
 
 const ProfileDashboard = () => {
     return (
         <Dashboard classes="boss-page-dashboard_page_profile">
-            <ProfileDashboardGroup />
+            <DashboardGroup>
+                <UserSummary />
+                <div className="boss-page-dashboard__buttons-group">
+                    <Link
+                        to="#"
+                        className="boss-button boss-button_role_edit boss-page-dashboard__button">
+                        Edit Profile
+                    </Link>
+                    <Link
+                        to="#"
+                        className="boss-button boss-button_role_block boss-page-dashboard__button">
+                        Disable Staff Member
+                    </Link>
+                </div>
+            </DashboardGroup>
             <TabContainer />
         </Dashboard>
 
